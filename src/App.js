12e59b86e3fbd5b3e991555f2a17b344749c1a51
yogesh123098 <yogeshpant293@gmail.com>
2020-09-26 22:04:01 +0545

    minor bug fixes

--- src/App.js
+++ src/App.js
@@ -127,7 +127,7 @@ function App() {
                         />
                       )}
                     />
-                    {/* <Redirect to="/" /> */}
+                    <Redirect to="/" />
                   </Switch>
                 </AnimatePresence>
               )}
