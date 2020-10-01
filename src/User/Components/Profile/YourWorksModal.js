62e094ef2a685cf2db12519a22a707e3a0415c2d
yogesh123098 <yogeshpant293@gmail.com>
2020-10-01 20:36:11 +0545

    minor bug fixes v5

--- src/User/Components/Profile/YourWorksModal.js
+++ src/User/Components/Profile/YourWorksModal.js
@@ -93,8 +93,8 @@ const YourWorksModal = ({
             <div className="your-works">
               <div className="your-work">
                 <h3>Submission Attachment</h3>
+                <h3 className="feedback">Description</h3>
                 <h3 className="grades">Grades</h3>
-                {/* <h3 className="feedback">Feedback</h3> */}
               </div>
               {submissions.map((submission) => (
                 <div key={submission.submission._id} className="your-work">
@@ -104,12 +104,12 @@ const YourWorksModal = ({
                   >
                     {submission.submission.attachments[0]?.name}
                   </a>
+                  <h3 className="feedback">
+                    {submission.submission.description || "-"}
+                  </h3>
                   <h3 className="grades">
                     {submission.submission.obtainedGrade || "-"}
                   </h3>
-                  {/* <h3 className="feedback">
-                    {submission.submission.feedback || "-"}
-                  </h3> */}
                 </div>
               ))}
             </div>
